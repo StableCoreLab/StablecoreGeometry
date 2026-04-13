b131cd6e5fb258091bde1ae956335b6273752da8
zhanghongwei <zhanghongwei@yjk.cn>
2026-04-13 08:57:15 +0800

    将误放在根目录下的文件移到本地

--- cmake/SCTestHelpers.cmake
+++ cmake/SCTestHelpers.cmake
@@ -1,1 +1,16 @@
-include(${CMAKE_CURRENT_LIST_DIR}/../../cmake/SCTestHelpers.cmake)
+function(SCCopyRuntimeDependenciesToTarget destination_target source_target)
+    if (NOT WIN32)
+        return()
+    endif()
+
+    add_custom_command(TARGET ${destination_target} POST_BUILD
+        COMMAND ${CMAKE_COMMAND} -E copy_if_different
+            $<TARGET_FILE:${source_target}>
+            $<TARGET_FILE_DIR:${destination_target}>
+        COMMAND ${CMAKE_COMMAND} -E copy_if_different
+            $<TARGET_RUNTIME_DLLS:${source_target}>
+            $<TARGET_FILE_DIR:${destination_target}>
+        COMMAND_EXPAND_LISTS
+        VERBATIM
+    )
+endfunction()
